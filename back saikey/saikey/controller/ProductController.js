d19fbabc2f1e6d0fbe0ea1f572e5965258e0eede
Samip-Subedi <samipsubedi68@gmail.com>
2025-08-02 17:13:39 +0545

    product update

--- back saikey/saikey/controller/ProductController.js
+++ back saikey/saikey/controller/ProductController.js
@@ -175,7 +175,7 @@ exports.createProductReview = catchAsyncErrors(async(req, res, next) => {
         success: true,
     });
 });
-
+// review all products
 // get all reviews of single product
 exports.getSingleProductReviews = catchAsyncErrors(async(req, res, next) => {
     const product = await Product.findById(req.query.id);
